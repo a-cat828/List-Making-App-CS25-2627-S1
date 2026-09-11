2c03ed11fa8005f83fdc74bd27e2adb94a2c20a3
a-cat828 <RyckardM017@stemcollegiate.ca>
2026-09-11 13:41:03 -0600

    almost done

--- Making App.py
+++ Making App.py
@@ -5,15 +5,19 @@ while not exitt:
     is_where_inportan = input("do you care where it is in the list? (y/n)")
     reassign_yes_or_no = input("do you whant to reassign one of your favorite movie?/shows?(y/n)")
     remove_yes_or_no = input("do you want to remove one of your favorite movie?/shows?(y/n)")
-    
+    check_yes_no = input("do you want to check if something is in the list?(y/n)")
+    check_numb_of = input("what do you want to check how many are in the list?(y/n) ")
     if is_where_inportan == "y":
          where= int(input("enter where in the list?(number)"))
          thing_add_to_list = input("what is one of your favorite movie/shows?(say exit to exit)")
          if thing_add_to_list == "exit":
              exitt = True
          else:
-            where= where - 1
-            favort.insert(where,thing_add_to_list)
+            if where <= len(favort):
+                where= where - 1
+                favort.insert(where,thing_add_to_list)
+            else:
+                print("invalid input")
             for i in favort:
                  print(favort[numb], numb + 1)
                  numb = numb + 1
@@ -24,8 +28,11 @@ while not exitt:
         if to_make_into == "exit":
             exitt = True
         else:
-            where = where - 1
-            favort[where]= to_make_into
+            if where <= len(favort):
+                where = where - 1
+                favort[where]= to_make_into
+            else:
+                print("invalid input")
             for i in favort:
                 print(favort[numb], numb + 1)
                 numb = numb + 1
@@ -33,12 +40,34 @@ while not exitt:
     elif remove_yes_or_no == "y":
         where = int(input("enter what in the list you want to remove?(number)"))
         where = where - 1
-        favort.pop(where)
+        if where <= len(favort):
+            favort.pop(where)
+        else:
+            print("invalid input")
         for i in favort:
             print(favort[numb], numb + 1)
             numb = numb + 1
         numb = 0
-    elif is_where_inportan == "n" and reassign_yes_or_no == "n" :
+    elif check_yes_no == "y":
+        where = (input("what do you want to check if it is in the list?(say exit to exit) "))
+        if where == "exit":
+            exitt = True
+        else:
+            if where in favort:
+                print("it is in the list")
+            else:
+                print("it is not in the list")
+            for i in favort:
+                print(favort[numb], numb + 1)
+                numb=numb+1
+            numb = 0
+    elif check_numb_of == "y":
+        print("there are", (len(favort)), "favorite movies/shows")
+        for i in favort:
+            print(favort[numb], numb + 1)
+            numb = numb + 1
+        numb = 0
+    elif is_where_inportan == "n" and reassign_yes_or_no == "n" and remove_yes_or_no == "n" and check_yes_no == "n" and check_numb_of == "n":
         thing_add_to_list = input("what is one of your favorite movie/shows?(say exit to exit)")
         if thing_add_to_list == "exit":
             exitt = True
@@ -48,3 +77,4 @@ while not exitt:
                 print(favort[numb], numb + 1)
                 numb=numb+1
             numb = 0
+    copy = favort.copy()
