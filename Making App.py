98b8b059e7a18bff58c05a7a757be8865d6337c5
a-cat828 <RyckardM017@stemcollegiate.ca>
2026-09-12 15:15:27 -0600

    done

--- Making App.py
+++ Making App.py
@@ -1,18 +1,21 @@
 favort = []
 exitt = False
 numb= 0
+copy = favort.copy()
 while not exitt:
     is_where_inportan = input("do you care where it is in the list? (y/n)")
     reassign_yes_or_no = input("do you whant to reassign one of your favorite movie?/shows?(y/n)")
     remove_yes_or_no = input("do you want to remove one of your favorite movie?/shows?(y/n)")
     check_yes_no = input("do you want to check if something is in the list?(y/n)")
     check_numb_of = input("what do you want to check how many are in the list?(y/n) ")
+    undo=input("undo?(y/n)")
     if is_where_inportan == "y":
          where= int(input("enter where in the list?(number)"))
          thing_add_to_list = input("what is one of your favorite movie/shows?(say exit to exit)")
          if thing_add_to_list == "exit":
              exitt = True
          else:
+            copy = favort.copy()
             if where <= len(favort):
                 where= where - 1
                 favort.insert(where,thing_add_to_list)
@@ -28,6 +31,7 @@ while not exitt:
         if to_make_into == "exit":
             exitt = True
         else:
+            copy = favort.copy()
             if where <= len(favort):
                 where = where - 1
                 favort[where]= to_make_into
@@ -38,6 +42,7 @@ while not exitt:
                 numb = numb + 1
             numb = 0
     elif remove_yes_or_no == "y":
+        copy = favort.copy()
         where = int(input("enter what in the list you want to remove?(number)"))
         where = where - 1
         if where <= len(favort):
@@ -49,6 +54,7 @@ while not exitt:
             numb = numb + 1
         numb = 0
     elif check_yes_no == "y":
+        copy = favort.copy()
         where = (input("what do you want to check if it is in the list?(say exit to exit) "))
         if where == "exit":
             exitt = True
@@ -67,14 +73,20 @@ while not exitt:
             print(favort[numb], numb + 1)
             numb = numb + 1
         numb = 0
-    elif is_where_inportan == "n" and reassign_yes_or_no == "n" and remove_yes_or_no == "n" and check_yes_no == "n" and check_numb_of == "n":
+    elif undo == "y":
+        favort = copy
+        for i in favort:
+            print(favort[numb], numb + 1)
+            numb = numb + 1
+        numb = 0
+    elif is_where_inportan == "n" and reassign_yes_or_no == "n" and remove_yes_or_no == "n" and check_yes_no == "n" and check_numb_of == "n" and undo == "n":
         thing_add_to_list = input("what is one of your favorite movie/shows?(say exit to exit)")
         if thing_add_to_list == "exit":
             exitt = True
         else:
+            copy = favort.copy()
             favort.append(thing_add_to_list)
             for i in favort:
                 print(favort[numb], numb + 1)
                 numb=numb+1
             numb = 0
-    copy = favort.copy()
